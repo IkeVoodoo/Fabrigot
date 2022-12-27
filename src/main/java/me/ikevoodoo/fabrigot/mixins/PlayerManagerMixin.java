9ca047a1913fa7f6a79894cbfd41bd9c60551378
IkeVoodoo <simone.coletti07@gmail.com>
2022-12-27 09:00:23 +0100

    Improved PlayerJoinEvent, added PlayerQuitEvent

--- src/main/java/me/ikevoodoo/fabrigot/mixins/PlayerManagerMixin.java
+++ src/main/java/me/ikevoodoo/fabrigot/mixins/PlayerManagerMixin.java
@@ -8,14 +8,17 @@ import net.minecraft.text.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerManager.class)
-public class PlayerManagerMixin {
+public abstract class PlayerManagerMixin {
+
+    @Shadow public abstract void broadcast(Text message, boolean overlay);
 
     @Unique
     private final ThreadLocal<ServerPlayerEntity> joiningPlayer = new ThreadLocal<>();
@@ -25,12 +28,15 @@ public class PlayerManagerMixin {
         this.joiningPlayer.set(player);
     }
 
-    @ModifyArg(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V", ordinal = 0))
-    private Text playerJoinEvent(Text message) {
+    @Redirect(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V"))
+    private void playerJoinEvent(PlayerManager instance, Text message, boolean overlay) {
         var event = new PlayerJoinEvent(Data.FABRIGOT_SERVER.getPlayer(this.joiningPlayer.get()), message.getString());
         this.joiningPlayer.remove();
         Bukkit.getPluginManager().callEvent(event);
-        return Text.literal(event.getJoinMessage());
+        if (event.getJoinMessage() != null && !event.getJoinMessage().isBlank()) {
+            var newText = Text.literal(event.getJoinMessage());
+            this.broadcast(newText, overlay);
+        }
     }
 
 }
--- src/main/java/me/ikevoodoo/fabrigot/mixins/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/me/ikevoodoo/fabrigot/mixins/ServerPlayNetworkHandlerMixin.java
@@ -3,14 +3,20 @@ package me.ikevoodoo.fabrigot.mixins;
 import me.ikevoodoo.fabrigot.Data;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.HashSet;
@@ -21,6 +27,8 @@ public abstract class ServerPlayNetworkHandlerMixin {
     @Shadow public ServerPlayerEntity player;
 
     @Shadow protected abstract SignedMessage getSignedMessage(ChatMessageC2SPacket packet);
+
+    @Shadow @Final private MinecraftServer server;
 
     @Inject(method = "onChatMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;canAcceptMessage(Ljava/lang/String;Ljava/time/Instant;Lnet/minecraft/network/message/LastSeenMessageList$Acknowledgment;)Z"), cancellable = true)
     private void handleMessage(ChatMessageC2SPacket packet, CallbackInfo ci) {
@@ -38,6 +46,16 @@ public abstract class ServerPlayNetworkHandlerMixin {
         }
 
         // TODO allow editing
+    }
+
+    @Redirect(method = "onDisconnected", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Z)V"))
+    private void playerDisconnected(PlayerManager instance, Text message, boolean overlay) {
+        var event = new PlayerQuitEvent(Data.FABRIGOT_SERVER.getPlayer(this.player), message.getString());
+        Bukkit.getPluginManager().callEvent(event);
+        if (event.getQuitMessage() != null && !event.getQuitMessage().isBlank()) {
+            var newText = Text.literal(event.getQuitMessage());
+            this.server.getPlayerManager().broadcast(newText, overlay);
+        }
     }
 
 }
